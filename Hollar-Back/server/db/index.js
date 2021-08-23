f74934bc68748545d82bfc011e051933da6bbc81
Drew Harthcock <drewerd15@gmail.com>
2021-08-23 15:24:16 -0400

    Buit db model for chat rooms and added seed data

--- Hollar-Back/server/db/index.js
+++ Hollar-Back/server/db/index.js
@@ -1,14 +1,18 @@
 //this is the access point for all things database related!
-
-const db = require('./db')
-
-const User = require('./models/User')
-
+const db = require("./db");
+const User = require("./models/User");
+const Event = require("./models/Event");
+const Message = require("./models/Message");
 //associations could go here!
-
+User.hasMany(Message);
+Message.belongsTo(User);
+Event.hasMany(Message);
+Message.belongsTo(Event);
 module.exports = {
   db,
   models: {
     User,
+    Event,
+    Message,
   },
-}
+};
