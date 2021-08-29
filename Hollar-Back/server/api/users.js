629174166da467ef7e90e532f1e1bc456899dc2f
Drew <drewerd15@gmail.com>
2021-08-29 18:24:37 -0400

    realized that this was harder than we thought lol

--- Hollar-Back/server/api/users.js
+++ Hollar-Back/server/api/users.js
@@ -1,47 +1,68 @@
-const router = require('express').Router()
-const { models: { User }} = require('../db')
-module.exports = router
+const router = require("express").Router();
+const {
+  models: { User },
+} = require("../db");
+module.exports = router;
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'username']
-    })
-    res.json(users)
+      attributes: ["id", "username"],
+    });
+    res.json(users);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-router.get('/login', async (req, res, next) => {
+router.get("/login", async (req, res, next) => {
   try {
     const user = await User.findOne({
       where: {
         username: req.headers.username,
-        password: req.headers.password
-
-      }
-    })
-    res.json(user)
+        password: req.headers.password,
+      },
+    });
+    res.json(user);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-router.get("/:userId/events", async (req,res,next) => {
+router.get("/:userId/events", async (req, res, next) => {
   try {
     //console.log("userId in backend", req.params.id);
     const user = await User.findByPk(req.params.userId);
 
-    res.send (await user.getEvents());
+    res.send(await user.getEvents());
   } catch (error) {
     //console.log('stuffs broke yo' + error);
-    next(error)
+    next(error);
   }
-} )
+});
+
+router.get("/:userId/events/directmessage", async (req, res, next) => {
+  try {
+    //console.log("userId in backend", req.params.id);
+    const directMessage = await Event.findOrCreate({
+      where: { eventObjectType: "dm" },
+      include: {
+        model: Attendees,
+        where: {
+          id: req.params.userId,
+        },
+      },
+    });
+
+    res.send(directMessage);
+  } catch (error) {
+    //console.log('stuffs broke yo' + error);
+    next(error);
+  }
+});
 
 // router.get('/:id', async (req, res, next) => {
 //   try {
@@ -57,12 +78,12 @@ router.get("/:userId/events", async (req,res,next) => {
 //   }
 // })
 
-router.post('/', async (req,res,next) => {
+router.post("/", async (req, res, next) => {
   try {
-    const newUser = await User.create(req.body)
-    console.log("newUser", newUser)
-    res.json(newUser)
+    const newUser = await User.create(req.body);
+    console.log("newUser", newUser);
+    res.json(newUser);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
