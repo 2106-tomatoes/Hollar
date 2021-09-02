857185cbb1b2e2965a987a796a2e956f2cb07c83
Drew <drewerd15@gmail.com>
2021-09-02 14:37:51 -0400

    initial commit for ui work

--- Hollar-Front/components/Screens/StartUp.js
+++ Hollar-Front/components/Screens/StartUp.js
@@ -1,14 +1,32 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
 
 export default function StartUp(props) {
   const history = props.history;
-  const navigation = useNavigation()
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
-      <Button title="Login" onPress={()=>navigation.navigate('Login')} />
-      <Button title="Sign Up" onPress={()=>navigation.navigate('Signup')} />
+      <View>
+        <Image
+          source={require("../../assets/Hollar.png")}
+          style={styles.logo}
+        />
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Login"
+            color="#669BBC"
+            onPress={() => navigation.navigate("Login")}
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Sign Up"
+            color="#669BBC"
+            onPress={() => navigation.navigate("Signup")}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -19,5 +37,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  }
+  },
+  buttonContainer: {
+    margin: 5,
+    // filter: drop - shadow("30px 10px 4px #4444dd"),
+  },
+  logo: {
+    height: 300,
+    width: 300,
+    // margin: 15,
+    // padding: 15,
+  },
 });
